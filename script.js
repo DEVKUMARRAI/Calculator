e9bde4b26452c441376e190b37566360021b9f1d
Dev <devrai197551@gmail.com>
2023-07-01 03:42:30 +0530

    structure created

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-function Add(a,b) 
-{
-    return a+b;
+function Add(a, b) {
+    return a + b;
 }
-function Subtract(a,b)
-{
-    return a-b;
+function Subtract(a, b) {
+    return a - b;
 }
-function Multiply(a,b)
-{
-    return a*b;
+function Multiply(a, b) {
+    return a * b;
 }
-function Divide(a,b)
-{
-    return a/b;
+function Divide(a, b) {
+    return a / b;
 }
-console.log(Divide(6,3));
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return Add(a, b);
+    }
+    else if (operator === '-') {
+        return Subtract(a, b);
+    }
+    else if (operator === '/') {
+        return Divide(a, b);
+    }
+    else if (operator === '*') {
+        return Multiply(a, b);
+    }
+}
