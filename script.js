efe1f50b252698267c00ad0bd9a5a0d19b939238
Dev <devrai197551@gmail.com>
2023-07-01 02:51:26 +0530

    arithematic functions created in js

---
+++ script.js
@@ -0,0 +1,17 @@
+function Add(a,b) 
+{
+    return a+b;
+}
+function Subtract(a,b)
+{
+    return a-b;
+}
+function Multiply(a,b)
+{
+    return a*b;
+}
+function Divide(a,b)
+{
+    return a/b;
+}
+console.log(Divide(6,3));
